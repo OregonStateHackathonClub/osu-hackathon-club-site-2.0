8899ac521e5099bf582b4f3e320976cf8b7a8ce7
r-petes <rachel.peterson.5683@gmail.com>
2022-01-26 18:02:01 -0700

    updated devpost link to main beaverhacks page

--- src/components/registration.js
+++ src/components/registration.js
@@ -6,14 +6,14 @@ function Registration() {
             <div className="infobody">
                 <h2>how to register:</h2>
                 {/* Update Devpost Link Each Hackathon */}
-                <p>To join the next hackathon, sign up for Beaverhacks on <button className="joinclubbuttons"><a href="" target="_blank" rel="noreferrer noopener">DevPost</a></button> and join the club via <button className="joinclubbuttons"><a href="https://apps.ideal-logic.com/osusli" target="_blank" rel="noreferrer noopener">IdealLogic</a></button></p>
+                <p>To join the next hackathon, sign up for Beaverhacks on <button className="joinclubbuttons"><a href="https://devpost.com/hackathons?organization=Oregon%20State%20University%20Hackathon%20Club" target="_blank" rel="noreferrer noopener">DevPost</a></button> and join the club via <button className="joinclubbuttons"><a href="https://apps.ideal-logic.com/osusli" target="_blank" rel="noreferrer noopener">IdealLogic</a></button></p>
 
             </div>
             <div className="infobody">
                 <h2>find your team: </h2>
 
                 {/* Update Devpost Link Each Hackathon */}
-                <p>Use <button className="joinclubbuttons"><a href="" target="_blank" rel="noreferrer noopener">DevPost</a></button>, <button className="joinclubbuttons"><a href="https://discord.gg/pCesGNGjPc" target="_blank" rel="noreferrer noopener">Discord</a></button>, or <button className="joinclubbuttons"><a href="https://osu-hackathon.slack.com/" target="_blank" rel="noreferrer noopener">Slack</a></button> to find teammates.</p>
+                <p>Use <button className="joinclubbuttons"><a href="https://devpost.com/hackathons?organization=Oregon%20State%20University%20Hackathon%20Club" target="_blank" rel="noreferrer noopener">DevPost</a></button>, <button className="joinclubbuttons"><a href="https://discord.gg/pCesGNGjPc" target="_blank" rel="noreferrer noopener">Discord</a></button>, or <button className="joinclubbuttons"><a href="https://osu-hackathon.slack.com/" target="_blank" rel="noreferrer noopener">Slack</a></button> to find teammates.</p>
             </div>
         </>
     );
