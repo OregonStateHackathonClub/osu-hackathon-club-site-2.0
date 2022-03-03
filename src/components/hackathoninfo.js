3fa9800835bd76b83c57d12971a8b4b22318c7ef
r-petes <rachel.peterson.5683@gmail.com>
2022-03-02 21:32:51 -0700

    updated info for the 2022 spring hackathon

--- src/components/hackathoninfo.js
+++ src/components/hackathoninfo.js
@@ -8,8 +8,16 @@ function Info () {
                 <h1>spring 2022 hackathon</h1>
                 <br/>
                 <h2> schedule </h2>
-                    <p className='orange-body-text'> TBD </p>
-                    
+
+                    <p className=''> Opening Ceremony Kick-off / Hackathon starts!:  </p>
+                    <p className='orange-body-text'> Wednesday, March 23rd - 5:00 PM PST (8:00 PM EST) </p>
+                    <br/>
+                    <p className=''> Hacking ends / Project Submission Deadline: </p>
+                    <p className='orange-body-text'> Sunday, March 27th - 3:00 PM PST (6:00 PM EST)</p>
+                    <br/>
+                    <p className=''> Project Demos / Q&A:</p>
+                    <p className='orange-body-text'> Sunday, March 27th - 5:00 PM PST (8:00 PM EST)</p>
+                    <br/>
                     {/* <p className='officer-and-judge-name'> The next hackathon will be from X to X */}
                     {/* The winners will be announced on X.</p> */}
                     
@@ -19,7 +27,7 @@ function Info () {
                     {/* <p className='officer-and-judge-name'> name, occupation </p> */}
 
                 <h2> theme </h2>
-                    <p className='orange-body-text'> TBD</p> 
+                    <p className='orange-body-text'> 🎮 Gamification!! 🎮 </p> 
                     
                     {/* <p className='officer-and-judge-name'> TBD </p> */}
 
