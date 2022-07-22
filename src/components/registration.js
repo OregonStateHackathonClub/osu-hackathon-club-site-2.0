45ff915db68eab799194e3ae3f1a960f4dbfea29
Chrissy <christinapersaud98@gmail.com>
2022-07-22 16:08:05 -0700

    Update registration.js

--- src/components/registration.js
+++ src/components/registration.js
@@ -6,7 +6,9 @@ function Registration() {
             <div className="infobody">
                 <h2>how to register:</h2>
                 {/* Update Devpost Link Each Hackathon */}
-                <p>To join the next hackathon, sign up for Beaverhacks on <button className="joinclubbuttons"><a href="https://devpost.com/hackathons?organization=Oregon%20State%20University%20Hackathon%20Club" target="_blank" rel="noreferrer noopener">DevPost</a></button> and join the club via <button className="joinclubbuttons"><a href="https://apps.ideal-logic.com/osusli" target="_blank" rel="noreferrer noopener">IdealLogic</a></button></p>
+                <p>To join the next hackathon, sign up for Beaverhacks on <button className="joinclubbuttons"><a href="https://devpost.com/hackathons?organization=Oregon%20State%20University%20Hackathon%20Club" target="_blank" rel="noreferrer noopener">DevPost</a></button>, 
+                    fill out the registration form <button className="joinclubbuttons"><a href="https://0of8oc0aue5.typeform.com/to/BJuNNkP3" target="_blank" rel="noreferrer noopener">Regsitration Form</a></button>
+                    and join the club via <button className="joinclubbuttons"><a href="https://apps.ideal-logic.com/osusli" target="_blank" rel="noreferrer noopener">IdealLogic</a></button></p>
 
             </div>
             <div className="infobody">
