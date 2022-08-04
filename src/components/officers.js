0a807fe8d289826f116769cc13a94c004867ca40
Chrissy <christinapersaud98@gmail.com>
2022-08-04 11:41:54 -0700

    Update officers.js

--- src/components/officers.js
+++ src/components/officers.js
@@ -30,7 +30,7 @@ const Officers = () => {
             </Row>
             <Row> 
                 <Col><p className='officer-title'> Treasurer: </p> </Col>
-                <Col><p className='officer-name'> Open position, reach out to oregonstatehackathonclub@gmail.com if interested </p> </Col> 
+                <Col><p className='officer-name'> Helena Lieu</p> </Col> 
             </Row>         
             <Row> 
                 <Col><p className='officer-title'> Communication Coordinator: </p></Col>
