485431bdd5fd826dbe9cf5cce74198c2cdca7495
Chrissy <christinapersaud98@gmail.com>
2022-08-10 18:26:27 -0700

    Update hackathoninfo.js

--- src/components/hackathoninfo.js
+++ src/components/hackathoninfo.js
@@ -19,13 +19,13 @@ function Info () {
                   alt="Fall 2022 hackathon"/>
                       
                     <p className=''> Opening Ceremony Kick-off / Hackathon starts!:  </p>
-                    <p className='orange-body-text'> Wednesday, March 23rd - 5:00 PM PST (8:00 PM EST) </p>
+                    <p className='orange-body-text'> Thursday, October 6th - 5:00 PM PST (8:00 PM EST) </p>
                     <br/>
                     <p className=''> Hacking ends / Project Submission Deadline: </p>
-                    <p className='orange-body-text'> Sunday, March 27th - 3:00 PM PST (6:00 PM EST)</p>
+                    <p className='orange-body-text'> Sunday, October 9th - 3:00 PM PST (6:00 PM EST)</p>
                     <br/>
                     <p className=''> Project Demos / Q&A:</p>
-                    <p className='orange-body-text'> Sunday, March 27th - 5:00 PM PST (8:00 PM EST)</p>
+                    <p className='orange-body-text'> Sunday, October 9th - 5:00 PM PST (8:00 PM EST)</p>
                     <br/>
                     {/* <p className='officer-and-judge-name'> The next hackathon will be from X to X */}
                     {/* The winners will be announced on X.</p> */}
