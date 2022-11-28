526cd0e26718731ae0a161a70797d55e7eb59d93
Chrissy <christinapersaud98@gmail.com>
2022-11-27 21:08:42 -0800

    Update hackathoninfo.js

--- src/components/hackathoninfo.js
+++ src/components/hackathoninfo.js
@@ -1,47 +1,35 @@
 import React from 'react';
-import fall2022posternew from '../images/fall2022posternew.png'
+import beaverhackswinter23 from '../images/beaverhackswinter23.png'
 
 function Info () {
 
     return (
         <>
             <div className="infobody">
-                <h1>Fall 2022 Hackathon</h1>
+                <h1>Winter 2023 Hackathon</h1>
                  <img
                   className="carousel-images"
                   src={fall2022posternew}
-                  alt="fall 2022 hackathon"/>
+                  alt="Winter 2023 hackathon"/>
                       
                 <br/>
                 <h2> schedule </h2>
                 <br/>                       
                     <p className=''> Opening Ceremony Kick-off / Hackathon starts!:  </p>
-                    <p className='orange-body-text'> Thursday, October 6th - 5:00 PM PST (8:00 PM EST) </p>
+                    <p className='orange-body-text'> TBD </p>
                     <br/>
                     <p className=''> Hacking ends / Project Submission Deadline: </p>
-                    <p className='orange-body-text'> Sunday, October 9th - 3:00 PM PST (6:00 PM EST)</p>
+                    <p className='orange-body-text'> TBD</p>
                     <br/>
                     <p className=''> Project Demos / Q&A:</p>
-                    <p className='orange-body-text'> Sunday, October 9th - 5:00 PM PST (8:00 PM EST)</p>
+                    <p className='orange-body-text'> TBD </p>
                     <br/>
                     {/* <p className='officer-and-judge-name'> The next hackathon will be from X to X */}
                     {/* The winners will be announced on X.</p> */}
                     
                 <h2> judges</h2>
                     <br/> 
-                    <p className='orange-body-text'>  David Rider @ Microsoft, Software Engineer</p> 
-                    <br/> 
-                    <p className='orange-body-text'>   Nate Kimball @ Microsoft, Software Engineer</p> 
-                    <br/> 
-                    <p className='orange-body-text'>  Jonah Spencer @ GitHub, Software Engineer</p> 
-                    <br/> 
-                    <p className='orange-body-text'>  Zaki Ahmed @ Amazon, Software Engineer</p> 
-                    <br/> 
-                    <p className='orange-body-text'>  Aaron Ennis @ Verizon, Software Engineer</p> 
-                    <br/> 
-                    <p className='orange-body-text'>  Wilkins White @ Daxbot, Senior Firmware Engineer</p> 
-                    <br/> 
-                        <p className='orange-body-text'> Jacob Eckroth @ Amazon, Software Engineer</p> 
+                    <p className='orange-body-text'>  TBD </p> 
                     <br/> 
 
                     
