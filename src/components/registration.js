c31158caa864d22be544037ffd5ec51fd515dafb
Chrissy <christinapersaud98@gmail.com>
2023-04-18 19:20:41 -0700

    Update registration.js

--- src/components/registration.js
+++ src/components/registration.js
@@ -7,7 +7,7 @@ function Registration() {
                 <h2>how to register:</h2>
                 {/* Update Devpost Link Each Hackathon */}
                 <p>To join the next hackathon, sign up for Beaverhacks on <button className="joinclubbuttons"><a href="https://devpost.com/hackathons?organization=Oregon%20State%20University%20Hackathon%20Club" target="_blank" rel="noreferrer noopener">DevPost</a></button>, fill out the registration form 
-                    <button className="joinclubbuttons"><a href="https://docs.google.com/forms/d/e/1FAIpQLScnCQo4M1NXw6Yt4nPIMq8CiY3FfS0AIKqRqKsZufQ0hZCX1g/viewform" target="_blank" rel="noreferrer noopener"> Google Form </a></button>
+                    <button className="joinclubbuttons"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfEvk6Fpxq4cQaWubiYSIWUQv-bJaCZYXZ1oKwMWO2o5FVi8g/viewform" target="_blank" rel="noreferrer noopener"> Google Form </a></button>
                     and join the club via <button className="joinclubbuttons"><a href="https://apps.ideal-logic.com/osusli" target="_blank" rel="noreferrer noopener">IdealLogic</a></button></p>
 
             </div>
@@ -15,7 +15,7 @@ function Registration() {
                 <h2>find your team: </h2>
 
                 {/* Update Devpost Link Each Hackathon */}
-                <p>Use <button className="joinclubbuttons"><a href="https://beaverhacks-winter2023.devpost.com/" target="_blank" rel="noreferrer noopener">DevPost</a></button>, <button className="joinclubbuttons"><a href="https://discord.gg/pCesGNGjPc" target="_blank" rel="noreferrer noopener">Discord</a></button>, or <button className="joinclubbuttons"><a href="https://osu-hackathon.slack.com/" target="_blank" rel="noreferrer noopener">Slack</a></button> to find teammates.</p>
+                <p>Use <button className="joinclubbuttons"><a href="https://beaver-hacks-spring-2023.devpost.com/" target="_blank" rel="noreferrer noopener">DevPost</a></button>, <button className="joinclubbuttons"><a href="https://discord.gg/pCesGNGjPc" target="_blank" rel="noreferrer noopener">Discord</a></button>, or <button className="joinclubbuttons"><a href="https://osu-hackathon.slack.com/" target="_blank" rel="noreferrer noopener">Slack</a></button> to find teammates.</p>
             </div>
         </>
     );
