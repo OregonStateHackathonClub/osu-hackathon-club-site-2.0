8668659323963f38ab56d39180eb10581e30a21a
Chrissy <christinapersaud98@gmail.com>
2023-04-21 18:29:58 -0700

    Update hackathoninfo.js

--- src/components/hackathoninfo.js
+++ src/components/hackathoninfo.js
@@ -16,20 +16,20 @@ function Info () {
                 <h2> schedule </h2>
                 <br/>                       
                     <p className=''> Opening Ceremony Kick-off / Hackathon starts!:  </p>
-                    <p className='orange-body-text'> January 12,2023 </p>
+                    <p className='orange-body-text'> May 19, 2023 (10 AM PST)</p>
                     <br/>
                     <p className=''> Hacking ends / Project Submission Deadline: </p>
-                    <p className='orange-body-text'> January 15, 2023</p>
+                    <p className='orange-body-text'> May 21, 2023 (8:30 PM PST) </p>
                     <br/>
                     <p className=''> Project Demos / Q&A:</p>
-                    <p className='orange-body-text'> January 15, 2023 </p>
+                    <p className='orange-body-text'> No project demos this time. </p>
                     <br/>
                     {/* <p className='officer-and-judge-name'> The next hackathon will be from X to X */}
-                    {/* The winners will be announced on X.</p> */}
+                    <p className='orange-body-text'>The winners will be announced on X.</p>
                     
                 <h2> judges</h2>
                     <br/> 
-                    <p className='orange-body-text'>  TBD </p> 
+                    <p className='orange-body-text'>  This hackathon will be judged by other students and hackathon participants. Students will vote on projects ( you are not allowed to vote on your teams project). The team with the most votes will receive the first place prize, the second most votes will receive the second place prize and the third most votes will receive the 3rd place prize! Please see Judging Criteria at the bottom of this page.  </p> 
                     <br/> 
 
                     
@@ -37,7 +37,7 @@ function Info () {
 
                 <h2> theme </h2>
                     <br/> 
-                    <p className='orange-body-text'> The theme will be released the day of the hackathon! </p> 
+                    <p className='orange-body-text'>LEVERAGING A.I. TOOLS</p> 
                     
                     {/* <p className='officer-and-judge-name'> TBD </p> */}
 
