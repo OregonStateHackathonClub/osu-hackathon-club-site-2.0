051881c0624ff3a0f8867672864e92792e6b6da8
Chrissy <christinapersaud98@gmail.com>
2023-04-27 09:51:48 -0700

    Update categoriesandprizes.js

--- src/components/categoriesandprizes.js
+++ src/components/categoriesandprizes.js
@@ -5,35 +5,7 @@ function CategoriesAndPrizes () {
     return ( 
         
 
-        <> 
-         /*
-            <h1>categories</h1>
-            <div className="infobody">
-                
-                <h2> overall category</h2>          
-                    <p> 
-                        All team members are undergrad OSU students
-                    </p>
-                    <br/> 
-
-                <h2>new student category</h2> 
-                    <p> 
-                    All team members are undergrad OSU students AND have not taken any 300-level courses. 
-                    Teams that are eligible for this prize are also eligible for the Overall prize.
-                    </p> 
-                    <br/> 
-
-                <h2>graduate students / alumni category</h2> 
-                    <p> 
-                    At least one team member is an OSU alumni/grad student. Due to funding restrictions, 
-                    there is no cash prize for this competition category, however winners will receive physical swag.            
-                    </p> 
-                    <br/> 
-
-                
-            </div>
-            */
-        
+        <>
             <h1>prizes</h1>
             <div className="infobody">
                     <h2>first place - overall</h2>
@@ -42,14 +14,7 @@ function CategoriesAndPrizes () {
                         This prize goes to the highest-scoring submission. It is available to all undergraduate 
                         teams including new student teams. 
                         </p>
-                        /*
-                        <p> 
-                        This prize goes to the highest-scoring submission. It is available to all undergraduate 
-                        teams including new student teams. Graduate and alumni teams are ineligible for this prize. 
-                        If a new student team wins this prize, they are ineligible for the First Place New Student 
-                        prize.                    
-                        </p>
-                        */
+                  
                         <br/> 
 
                     <h2> second place - overall</h2>
@@ -57,15 +22,6 @@ function CategoriesAndPrizes () {
                         This prize goes to the second highest-scoring submission. It is available to all undergraduate 
                         teams including new student teams. 
                         </p>
-        
-                        /* 
-                        <p>
-                        This prize goes to the second highest-scoring submission. It is available to all 
-                        undergraduate teams including new student teams. Graduate and alumni teams are ineligible
-                        for this prize. If a new student team wins this prize, they are ineligible for the First 
-                        Place New Student prize.                    
-                        </p> 
-                        */
                         <br/> 
 
                     <h2> third place - overall</h2>
@@ -74,23 +30,7 @@ function CategoriesAndPrizes () {
                         This prize goes to the third highest-scoring submission. It is available to all undergraduate 
                         teams including new student teams. 
                         </p>
-        
-                         /*
-                         <p> 
-                        This prize goes to the third highest-scoring submission. It is available to all undergraduate
-                        teams including new student teams. Graduate and alumni teams are ineligible for this prize. 
-                        If a new student team wins this prize, they are ineligible for the First Place New Student 
-                        prize.                    
-                        </p>
-                        */
                         <br/> 
-                    /*
-                        <h2>new student</h2>
-                        <p> 
-                        This award goes to the highest-scoring new student. Teams that won any of the "Overall" prizes are ineligible for this prize. 
-                        </p>
-                        <br/>
-                     */
 
             </div>   
 
