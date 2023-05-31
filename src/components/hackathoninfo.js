1344e3365d365ed5ab68b707f61dcb31b50746fd
Chrissy <christinapersaud98@gmail.com>
2023-05-31 14:23:03 -0700

    Update hackathoninfo.js

--- src/components/hackathoninfo.js
+++ src/components/hackathoninfo.js
@@ -6,30 +6,26 @@ function Info () {
     return (
         <>
             <div className="infobody">
-                <h1>Spring 2023 Hackathon</h1>
-                 <img
-                  className="carousel-images"
-                  src={spring2023squarelogo}
-                  alt="Spring 2023 hackathon"/>
+                <h1>To Be Announced</h1>
                       
                 <br/>
                 <h2> schedule </h2>
                 <br/>                       
                     <p className=''> Opening Ceremony Kick-off / Hackathon starts!:  </p>
-                    <p className='orange-body-text'> May 19, 2023 (10 AM PST)</p>
+                    <p className='orange-body-text'>TBA</p>
                     <br/>
                     <p className=''> Hacking ends / Project Submission Deadline: </p>
-                    <p className='orange-body-text'> May 21, 2023 (8:30 PM PST) </p>
+                    <p className='orange-body-text'>TBA </p>
                     <br/>
                     <p className=''> Project Demos / Q&A:</p>
-                    <p className='orange-body-text'> No project demos this time. </p>
+                    <p className='orange-body-text'>TBA </p>
                     <br/>
                     {/* <p className='officer-and-judge-name'> The next hackathon will be from X to X */}
-                    <p className='orange-body-text'>The winners will be announced on May 28, 2023.</p>
+                    <p className='orange-body-text'>TBA</p>
                     
                 <h2> judges</h2>
                     <br/> 
-                    <p className='orange-body-text'>  This hackathon will be judged by other students and hackathon participants. Students will vote on projects ( you are not allowed to vote on your teams project). The team with the most votes will receive the first place prize, the second most votes will receive the second place prize and the third most votes will receive the 3rd place prize! Please see Judging Criteria at the bottom of this page.  </p> 
+                    <p className='orange-body-text'>TBA </p> 
                     <br/> 
 
                     
@@ -37,7 +33,7 @@ function Info () {
 
                 <h2> theme </h2>
                     <br/> 
-                    <p className='orange-body-text'>LEVERAGING A.I. TOOLS</p> 
+                    <p className='orange-body-text'>TBA</p> 
                     
                     {/* <p className='officer-and-judge-name'> TBD </p> */}
 
