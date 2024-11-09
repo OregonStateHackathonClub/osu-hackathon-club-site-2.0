6afa11a05cf04caae967e795befd1c5d0cda3415
owenkrause <owenjkrause@gmail.com>
2024-11-09 11:51:40 -0800

    finish basic animation

--- website/src/components/animation.tsx
+++ website/src/components/animation.tsx
@@ -9,7 +9,6 @@ interface point {
 }
 
 export const GridAnimation = () => {
-
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const requestIdRef = useRef<number | null>(null);
 
@@ -36,27 +35,99 @@ export const GridAnimation = () => {
     const width = canvas.width / 2;
     const height = canvas.height / 2;
 
+    function drawLine(ctx: CanvasRenderingContext2D, x: number, y: number, length: number, angle: number) {
+      const endX = x + length * Math.cos(-angle * Math.PI / 180);
+      const endY = y + length * Math.sin(-angle * Math.PI / 180);
+      
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(endX, endY);
+      ctx.stroke();
+    }
+    
+    ctx.strokeStyle = "white"
+    ctx.font = "16px Monospace";
+
     let frame = 0;
+    let updates = 0;
+    let lastUpdateFrame = 0;
+
+    const s = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+    const lines = [
+      " ___                        _   _         _",
+      "| - )___ __ _ _ __ __ _ _  | |_| |__ _ __| |_ ___",
+      "| - / -_/ _\` \\ / / -_) '_| |  _  / _\` / _| / (_-/",
+      "|___\\___\\__,_|\\_/\\___|_|   |_| |_\\__,_\\__|_\\_/__/"
+    ]
 
     const render = () => {
       frame++;
 
       ctx.fillStyle = '#0a0a0a';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.font = "12px Monospace";
       ctx.fillStyle = "white";
 
+      // 150 to 450 out of the 600 x 600 canvas
       for (const point of grid) {
+
+        const dx = (point.x * width * 0.5 + width) - 300
+        const dy = (point.y * height * 0.5 + height) - 300
+
+        const dist = Math.sqrt(dx*dx + dy*dy)
+
+        const currAngle = Math.atan2(dy, dx)
+        const newAngle = currAngle - Math.pow(dist, 0.5) * frame * 0.0003
 
         const rotatedX = point.x * Math.cos(frame * .005 + Math.abs(point.x) + Math.abs(point.y)) - point.y * Math.sin(frame * .005 + Math.abs(point.x) + Math.abs(point.y));
         const rotatedY = point.x * Math.sin(frame * .005 + Math.abs(point.x) + Math.abs(point.y)) + point.y * Math.cos(frame * .005 + Math.abs(point.x) + Math.abs(point.y));
 
         const offsetPoint = {
-          x: rotatedX * (width * 0.5) + width,
-          y: rotatedY * (height * 0.5) + height
+          x: rotatedX * width * 0.5 + width,
+          y: rotatedY * height * 0.5 + height
         };
 
-        ctx.fillText(point.char, Math.round(offsetPoint.x / 10) * 10, Math.round(offsetPoint.y / 10) * 10);
+        const offsetPoint2 = {
+          x: 300 + Math.cos(newAngle) * dist,
+          y: 300 + Math.sin(newAngle) * dist
+        }
+
+        ctx.fillText(point.char, Math.round(offsetPoint2.x / 10) * 10, Math.round(offsetPoint2.y / 10) * 10);
+      }
+
+      ctx.fillStyle = '#0a0a0a';
+      ctx.fillRect(4, 276, 468, 52);
+      ctx.fillStyle = "white";
+
+      // Beaver Hacks ASCII Art: 56 chars long, 468 x 52 px
+      // drawLine(ctx, 4, 324, 468, 0)
+      // drawLine(ctx, 2, 268, 52, 270)
+
+      // ctx.fillText(" ___                        _   _         _", 66, 276)
+      // ctx.fillText("| - )___ __ _ _ __ __ _ _  | |_| |__ _ __| |_ ___", 66, 292)
+      // ctx.fillText("| - / -_/ _` \\ / / -_) '_| |  _  / _` / _| / (_-/", 66, 308)
+      // ctx.fillText("|___\\___\\__,_|\\_/\\___|_|   |_| |_\\__,_\\__|_\\_/__/", 66, 324)
+
+      if (updates < 20) {
+        console.log(1)
+        const framesBetweenUpdates = Math.ceil(1 + (updates / 95) * 10);
+        if (frame - lastUpdateFrame >= framesBetweenUpdates) {
+          for (let i = 0, n = lines.length; i < n; i++) {
+            const chars = lines[i].split("")
+            for (let j = 0, n = chars.length; j < n; j++) {
+              if (chars[j] == " " || chars[j] == "\n") continue
+              const charCode = ((frame + chars[j].charCodeAt(0)) % s.length) + 32
+              chars[j] = String.fromCharCode(charCode)
+            }
+            lines[i] = chars.join("")
+            ctx.fillText(lines[i], 66, 276 + i*16)
+            updates++
+            lastUpdateFrame = frame
+          }
+          
+        }
+      }
+      for (let i = 0, n = lines.length; i < n; i++) {
+        ctx.fillText(lines[i], 66, 276 + i*16)
       }
 
       requestIdRef.current = requestAnimationFrame(render);
--- website/src/components/apply.tsx
+++ website/src/components/apply.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
 
 export const ApplyButton = () => {
-    return <Button variant="outline" className="absolute top-4 right-4">Apply</Button>
+    return <Button variant="outline">Apply</Button>
 }
