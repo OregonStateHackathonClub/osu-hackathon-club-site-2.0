28d020545245e89f6302ca4b7b68a10041aa07d6
owenkrause <owenjkrause@gmail.com>
2024-11-12 23:28:45 -0800

    update provider redirect uri for build

--- src/lib/auth/providers.ts
+++ src/lib/auth/providers.ts
@@ -5,5 +5,5 @@ import { Google } from "arctic";
 export const google = new Google(
 	process.env.GOOGLE_CLIENT_ID!,
 	process.env.GOOGLE_CLIENT_SECRET!,
-	"http://localhost:3000/api/login/callback"
+	"https://beaverhacks.org/api/login/callback"
 );
