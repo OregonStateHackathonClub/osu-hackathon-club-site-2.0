496d1340a1e7922f159cb0e884ed17d0d6a9d8be
owenkrause <owenjkrause@gmail.com>
2024-11-12 21:18:12 -0800

    add countdown until hackathon

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { GridAnimation } from "@/components/animation";
 import { ChevronButton } from "@/components/chevron";
 import { Navbar } from "@/components/navbar";
+import { Countdown } from "@/components/countdown";
 
 const Home = () => {
 
@@ -15,9 +16,11 @@ const Home = () => {
         <GridAnimation />
         <ChevronButton targetRef={next}/>
       </div>
-      <div ref={next} className="w-screen h-screen flex items-center justify-center">
-        Oregon State's Biggest Hackathon
+      <div ref={next} className="w-screen h-screen flex flex-col items-center justify-center">
+        <span>Oregon State's Biggest Hackathon</span>
+        <Countdown targetDate={new Date(1732327200 * 1000)} />
       </div>
+      
     </>
   );
 }
---
+++ website/src/components/countdown.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
+export const Countdown = ({ targetDate }: { targetDate: Date }) => {
+  const [timeLeft, setTimeLeft] = useState<any>(null)
+
+  function calculateTimeLeft() {
+    const dt = +targetDate - +new Date()
+
+    if (dt <= 0) {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      }
+    }
+
+    return {
+      days: Math.floor(dt / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((dt / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((dt / 1000 / 60) % 60),
+      seconds: Math.floor((dt / 1000) % 60)
+    }
+  }
+
+  useEffect(() => {
+    setTimeLeft(calculateTimeLeft())
+
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+
+  // i want to add a number interpolation animation here
+  if (!timeLeft) return null
+
+  return (
+    <div className="flex gap-4">
+      <div>
+        <span className="font-bold text-2xl">{timeLeft.days}</span>
+        <span className="text-sm"> days</span>
+      </div>
+      <div>
+        <span className="font-bold text-2xl">{timeLeft.hours}</span>
+        <span className="text-sm"> hours</span>
+      </div>
+      <div>
+        <span className="font-bold text-2xl">{timeLeft.minutes}</span>
+        <span className="text-sm"> minutes</span>
+      </div>
+      <div>
+        <span className="font-bold text-2xl">{timeLeft.seconds}</span>
+        <span className="text-sm"> seconds</span>
+      </div>
+    </div>
+  )
+}
