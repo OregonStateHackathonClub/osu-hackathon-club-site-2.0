606d8ad706f48aae82219cbf6bf09f2f514d6c79
owenkrause <owenjkrause@gmail.com>
2024-11-14 23:12:23 -0800

    add redirectURI for prod and dev

--- src/lib/auth/providers.ts
+++ src/lib/auth/providers.ts
@@ -2,8 +2,12 @@ import { Google } from "arctic";
 
 // https://arcticjs.dev/providers/google
 
+const redirectURI = process.env.NODE_ENV === "production"
+  ? "https://beaverhacks.org/api/login/callback"
+  : "http://localhost:3000/api/login/callback";
+
 export const google = new Google(
 	process.env.GOOGLE_CLIENT_ID!,
 	process.env.GOOGLE_CLIENT_SECRET!,
-	"https://beaverhacks.org/api/login/callback"
+	redirectURI
 );
